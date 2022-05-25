811f5abe578acfbe5ae0a6de912bc0d0805c4676
yusufalmassady <yusufalmassady@gmail.com>
2022-05-26 01:29:19 +0200

    ADDED COMMENTS

--- PROJECT_TRAINEE/SECURITY.c
+++ PROJECT_TRAINEE/SECURITY.c
@@ -51,22 +51,30 @@ void ASECURITY_voidCheckPassword()
 	u8 Local_u8Key=NOT_PRESSED ,Local_u8Loop=0,Local_u8PassLoopCounter=0,Local_u8BuzzLoop=0,Local_u8CheckForError=0;
 	while(Local_u8CheckForError==0)
 	{
+    //REDEFINE KEY AS NOT PRESSED
 	Local_u8Key=NOT_PRESSED;
+	//DISPLAY MESSAGE FOR USER
 	LCD_vidwritecommand(lcd_clr);
 	LCD_setposition(0,0);
 	LCD_vidwritestringoncursor("ENTER YOUR PASS:");
 	LCD_setposition(1,5);
+	//LOOP TO PASS THROUGH EEPROM
 	for(Local_u8Loop=0;Local_u8Loop<4;Local_u8Loop++)
 		{
+		    //INFINITE LOOP TRAP UNTIL KEY IS PRESSED
 			while(Local_u8Key==NOT_PRESSED )
 			{
 				Local_u8Key=KPD_u8GetPressedKey();
 			}
+			//COMPARES USER DATA TO INPUT DATA
 			if(Local_u8Key!=EEpromReadByte(Local_u8Loop + 2))
 			{
+				//0 MEANS ERROR AND TRY AGAIN LOOP MUST CONTINUE
 				Local_u8CheckForError=0;
+				//CHECK FOR THREE TRIALS
 				if(Local_u8PassLoopCounter==3)
 				{
+					//USER IS INCORRECT THREE TIMES MESSAGE
 					LCD_vidwritecommand(lcd_clr);
 					LCD_setposition(0,0);
 					LCD_vidwritestringoncursor("STOP RIGHT THERE");
@@ -81,23 +89,29 @@ void ASECURITY_voidCheckPassword()
 						DIO_u8SetPinValue(DIO_u8PORTA,DIO_u8PIN6,DIO_u8PIN_LOW);
 							   _delay_ms(500);
 					}
+					//DEACTIVATE BUZZER
 					DIO_u8SetPinValue(DIO_u8PORTA,DIO_u8PIN6,DIO_u8PIN_LOW);
+					//EXIT FUNCTION
 					return;
 				}
 				else
 				{
+					//TRY GAIN MESSAGE
 					LCD_vidwritecommand(lcd_clr);
 					LCD_setposition(0,0);
 					LCD_vidwritestringoncursor("TRY AGAIN");
 					_delay_ms(1000);
+					//UNCHECK A TRY OF THE THREE
 					Local_u8PassLoopCounter++;
 				}
 
 			}
 			else
 			{
+				//USER WAS CORRECT BREAK LOOP
 				Local_u8CheckForError=1;
 			}
+			//RESET VALUE TO TRAP IN LOOP
 			Local_u8Key=NOT_PRESSED;
         }
 
